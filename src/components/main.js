ba3b1a3fb98edeb91078120e1b582bce8ec070f6
Muiruri Pennina <s1800083@student.businesscollege.fi>
2019-09-29 17:10:19 +0300

    added a fetch for five day forecast and a filter to limit results to return readings once a day

--- src/components/main.js
+++ src/components/main.js
@@ -3,21 +3,37 @@ import apiConfig from '../key.js'
 class Main extends Component {
   state={
     city:'helsinki',
+    id:658225,
+    dailyData:{},
+    weekData:{}
   }
   fetchData = () => {
     const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${this.state.city}&units=metric&APPID=${apiConfig.weatherKey}`;
     fetch(weatherUrl)
     .then(response => response.json())
     .then(data => {
-      console.log(data);
+      this.setState({ dailyData: data, id: data.id});
+      console.log('Day: '+ JSON.stringify(data));
+      console.log('Id: '+data.id);
+    })
+    .catch(error => console.log(error));
+  };
+  fetchWeekData = () => {
+    const weatherUrl = `https://api.openweathermap.org/data/2.5/forecast?id=${this.state.id}&units=metric&APPID=${apiConfig.weatherKey}`;
+    fetch(weatherUrl)
+    .then(response => response.json())
+    .then(weekdata => {
+      const dailyData = weekdata.list.filter(reading => reading.dt_txt.includes("18:00:00"))
+      this.setState({
+        weekData: dailyData
+      }, () => console.log(this.state))
     })
     .catch(error => console.log(error));
   };
   componentDidMount =()=>{
     this.fetchData();
+    this.fetchWeekData();
   }
-
-
   render() {
     return (
       <div>
