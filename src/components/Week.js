25ad69de829d82ad1495d8874b26136d97645ac7
Muiruri Pennina <s1800083@student.businesscollege.fi>
2019-10-07 17:52:14 +0300

    rendered date icon and description data from api

--- src/components/Week.js
+++ src/components/Week.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
+import moment from 'moment';
 import {Row,  Col} from "react-bootstrap";
 
 class Week extends Component {
   render() {
   console.log(this.props.reading);
+  console.log(this.props.reading.weather[0].description);
+
     return (
         <div className="week-card">
           <div className="float-left">
-            <p>Date</p>
-            <p><i></i>icon</p>
+            <p>{moment(this.props.reading.dt*1000).format("dddd")}</p>
+            <img src={`http://openweathermap.org/img/wn/${this.props.reading.weather[0].icon}@2x.png`} alt="weather-icon" className="img-responsive h-75"/>
           </div>
           <div className="float-right">
             <p>{this.props.reading.main.temp}°C</p>
-            <p className="float-left">description</p>
+            <p className="float-left">{this.props.reading.weather[0].description}</p>
           </div>
         </div>
     );
