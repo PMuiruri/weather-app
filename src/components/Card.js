e37095e46f81b3af49a585f05706fc8a21797192
Muiruri Pennina <s1800083@student.businesscollege.fi>
2019-10-09 21:27:48 +0300

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -13,22 +13,22 @@ class Card extends Component {
     return (
       <div>
         <Row>
-          <Col md={{span: 10, offset: 1}}>
+          <Col md={{span: 8, offset: 2}}>
             <div className=" card weather-card">
               <div className="top">
                 <div className="wrapper">
-                  <h1 className="heading">{this.props.name}</h1>
+                <img src={`http://openweathermap.org/img/wn/${this.props.icon}@2x.png`} alt="weather-icon" className="img-responsive float-right"/>
+                  <h1 className="heading font-weight-bold">{this.props.name}</h1>
                   <h3 className="location">{this.props.description}</h3>
-                  <img src={`http://openweathermap.org/img/wn/${this.props.icon}@2x.png`} alt="weather-icon" className="img-responsive"/>
-                  <p className="temp">
-                    <span className="temp-value">{this.props.temp} </span>
-                    <span className="temp-value">&deg;C</span>
+                  <p className="temp font-weight-bold">
+                    <span className="temp-value">{Math.round(this.props.temp)} </span>
+                    <span className="temp-value"> &deg;C</span>
                   </p>
                 </div>
               </div>
             </div>
           </Col>
-          <Col md={{span: 10, offset: 1}} >
+          <Col md={{span: 8, offset: 2}} >
             <div className="">
               {weekCards}
             </div>
