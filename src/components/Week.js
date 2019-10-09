acedc7d7b5163daa26c7b14efc19f2b715a1a5b4
Muiruri Pennina <s1800083@student.businesscollege.fi>
2019-10-09 21:25:27 +0300

    rounded the tempreture reading to give whole number

--- src/components/Week.js
+++ src/components/Week.js
@@ -8,10 +8,10 @@ class Week extends Component {
 
     return (
         <div className="week-card flex-column">
-            <p>{moment(this.props.reading.dt*1000).format("dddd")}</p>
-              <p>{this.props.reading.main.temp}°C</p>
+            <p className="font-weight-bold">{moment(this.props.reading.dt*1000).format("dddd")}</p>
+              <p>{Math.round(this.props.reading.main.temp)} °C</p>
               <i className={`owi owi-${this.props.reading.weather[0].icon} owi-5x`}></i>
-            <p className="">{this.props.reading.weather[0].description}</p>
+            <p className="font-weight-bold">{this.props.reading.weather[0].description}</p>
         </div>
     );
   }
