acbf5cba5302d77cdedce3174ff99b7509fa6853
Muiruri Pennina <s1800083@student.businesscollege.fi>
2019-10-10 17:06:46 +0300

    added function for submitting form and button

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,7 +4,7 @@ class Form extends Component {
 
   render() {
     return (
-      <form className="card card-sm card-form">
+      <form className="card card-sm card-form" onSubmit={this.props.handleSubmit}>
         <div className="card-body row no-gutters align-items-center">
           <div className="col-auto">
             <i className="fas fa-search h4 text-body"></i>
@@ -13,7 +13,7 @@ class Form extends Component {
             <input className="form-control search_fild" type="text" name="city" onChange={this.props.handleChange} value={this.props.city} placeholder="Search a City e.g Helsinki" />
           </div>
           <div className="col-auto">
-            <button className="btn btn-lg btn-danger search_btn" value="" onSubmit={this.props.handleSubmit} type="submit">Search</button>
+            <button className="btn btn-lg btn-danger search_btn" onSubmit={this.props.handleSubmit} type="submit">Search</button>
           </div>
         </div>
       </form>
