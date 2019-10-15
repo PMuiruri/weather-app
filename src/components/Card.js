e3fb918292a7bb8e81af6d791577d14e4a6386c1
Muiruri Pennina <s1800083@student.businesscollege.fi>
2019-10-15 14:26:25 +0300

    updated card to hold all dashboard compoenents

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,41 +2,33 @@ import React, { Component } from 'react';
 import {Row, Col} from "react-bootstrap";
 import Form from './Form.js';
 import Week from './Week.js';
+import Chart from './weatherChart.js';
+import WeatherCard from './weatherCard.js';
+import MiniCard from './miniCard';
 import '../App.css';
 
 class Card extends Component {
   render() {
-    let weekCards =this.props.weekdata.map((reading, index) => <Week reading={reading} key={index} />);
+    let weekCards = this.props.weekdata.map((reading, index) => <Week reading={reading} key={index} />);
+
     var data = (this.props);
-
     if(typeof(data) != 'undefined'){
-
     return (
       <div>
         <Row>
-          <Col md={{span: 8, offset: 2}}>
+          <Col md={{span: 8, offset:2}}>
             <Form handleSubmit={this.props.handleSubmit} handleChange={this.props.handleChange}/>
           </Col>
           <Col md={{span: 8, offset: 2}}>
-            <div className=" card weather-card">
-              <div className="top">
-                <div className="wrapper">
-                <img src={`http://openweathermap.org/img/wn/${this.props.icon}@2x.png`} alt="weather-icon" className="img-responsive float-right"/>
-                  <h1 className="heading font-weight-bold">{this.props.name}</h1>
-                  <h3 className="location">{this.props.description}</h3>
-                  <p className="temp font-weight-bold">
-                    <span className="temp-value">{Math.round(this.props.temp)} </span>
-                    <span className="temp-value"> &deg;C</span>
-                  </p>
-                </div>
-              </div>
-            </div>
+            <WeatherCard {...this.props}/>
+            <div className="">{weekCards}</div>
+            <Chart fulldata={this.props.fulldata} {...weekCards}/>
+            <MiniCard name={'Pressure'} reading={this.props.pressure} icon={'fas fa-wind'}/>
+            <MiniCard name={'Humidity'} reading={this.props.humidity} icon={'fas fa-tint'}/>
+            <MiniCard name={'Temperature Max'} reading={this.props.temp_max} icon={'fas fa-temperature-high'}/>
+            <MiniCard name={'Temperature Min'} reading={this.props.temp_min} icon={'fas fa-temperature-low'} />
           </Col>
-          <Col md={{span: 8, offset: 2}} >
-            <div className="">
-              {weekCards}
-            </div>
-          </Col>
+
         </Row>
       </div>
     );
