f213653b3fb0d45343ee70d5ba202b46ddb68398
Muiruri Pennina <s1800083@student.businesscollege.fi>
2019-10-15 14:27:17 +0300

    updated chart to a line chart instead of combo chart

--- src/components/weatherChart.js
+++ src/components/weatherChart.js
@@ -5,38 +5,41 @@ import moment from 'moment';
 class weatherChart extends Component {
 
   render() {
-    console.log(this.props.fulldata);
     const time = this.props.fulldata.map(list => list.dt_txt);
-    console.log(time);
     const temperature = this.props.fulldata.map(list => list.main.temp);
-    console.log(temperature);
 
     const chartData = [['Hour', 'Temperature']]
     for (let i = 0; i < this.props.fulldata.length; i += 1) {
       chartData.push([moment( time[i]).format('LT'), temperature[i]]);
     }
+
     return (
       <div style={{ display: 'flex', }}>
         <Chart
-          width={1245}
+          width={1275}
           height={450}
-          chartType="ComboChart"
+          chartType="LineChart"
           loader={<div>Loading Chart</div>}
           data={chartData}
           options={{
             title: 'Weather Forecast',
+            titleTextStyle: {color: '#FFF', fontSize: 20},
             chartArea: { width: '70%' },
             hAxis: {
               title: 'Time',
               minValue: 0,
+              titleTextStyle: {color: '#FFF', fontSize: 18, fontStyle:'normal'},
+              textStyle:{color: '#FFF'}
             },
             vAxis: {
               title: 'Temperature',
+              titleTextStyle: {color: '#FFF', fontSize: 18, fontStyle:'normal'},
+              textStyle:{color: '#FFF'}
             },
-            seriesType: 'bars',
-            series: { 5: { type: 'line' }},
-            colors: ['#d9534f'],
-            backgroundColor: '#E4E4E4',
+            legend: {textStyle: {color: '#fff', fontSize:18}},
+            seriesType: 'line',
+            colors: ['#d9534f', '#fff'],
+            backgroundColor: '#254059',
           }}
           legendToggle
         />
