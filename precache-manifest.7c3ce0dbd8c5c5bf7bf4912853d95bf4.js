c7da36ed776636b2c7165679772ee570fa670c42
PMuiruri <peninah.m.muiruri@gmail.com>
2019-12-04 17:11:03 +0200

    Updates

---
+++ precache-manifest.7c3ce0dbd8c5c5bf7bf4912853d95bf4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12f27032d902db15b690e7495569474e",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "3504ba70399dd2380a4c",
+    "url": "/weather-app/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "810d7f2ba776fb688b98",
+    "url": "/weather-app/static/css/main.0bdb7d22.chunk.css"
+  },
+  {
+    "revision": "3504ba70399dd2380a4c",
+    "url": "/weather-app/static/js/2.0b5e4444.chunk.js"
+  },
+  {
+    "revision": "810d7f2ba776fb688b98",
+    "url": "/weather-app/static/js/main.c2f580a6.chunk.js"
+  },
+  {
+    "revision": "baef7041c394808038d6",
+    "url": "/weather-app/static/js/runtime-main.f8fd2720.js"
+  },
+  {
+    "revision": "d920c599e5aa66ac201e15c1030fa938",
+    "url": "/weather-app/static/media/asset3.d920c599.png"
+  }
+]);
